cdffd9fd3ac3dea259c17306b19153e7781bdda3
Owen Lee Scott <fumwa23@outlook.com>
2021-10-21 20:40:14 +0800

    Initial Commit

---
+++ TwitterClone/TwitterCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterCloneApp.swift
+//  TwitterClone
+//
+//  Created by Owen LS on 21/10/21.
+//
+
+import SwiftUI
+
+@main
+struct TwitterCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
